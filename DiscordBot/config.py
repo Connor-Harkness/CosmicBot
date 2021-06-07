d8bb80990f38413d6f9c99d03021e6b784b84c7c
Connor Harkness <connor-harkness@outlook.com>
2021-06-07 21:05:58 +0100

    Starting work on Moderation Module

--- DiscordBot/config.py
+++ DiscordBot/config.py
@@ -1,5 +1,5 @@
 prefix="!"
-base_cogs="cogs.moderation"
+base_cogs="cogs.admin"
 token="Mzg0Mzk5NDE0NzIwMjAwNzI4.Whr92g.IwEbdvcD100Lb4t7_wYxdWCUCH0"
 
-CONSTANTS={"Staff Role":[]}
+CONSTANTS={"Staff Roles":[]}
